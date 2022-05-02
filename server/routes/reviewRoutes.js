b201d9049377ccbfd6ead3f4aa3f4eddcb12d397
CyberZenLabs <sam.singer79@gmail.com>
2022-05-02 19:53:38 +0700

    removed admin

--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -9,22 +9,12 @@ router.use(authController.protect);
 router
   .route('/')
   .get(reviewController.getAllReviews)
-  .post(
-    authController.restrictTo('user'),
-    reviewController.setTourUserIds,
-    reviewController.createReview
-  );
+  .post(reviewController.setTourUserIds, reviewController.createReview);
 
 router
   .route('/:id')
   .get(reviewController.getReview)
-  .patch(
-    authController.restrictTo('user', 'admin'),
-    reviewController.updateReview
-  )
-  .delete(
-    authController.restrictTo('user', 'admin'),
-    reviewController.deleteReview
-  );
+  .patch(reviewController.updateReview)
+  .delete(reviewController.deleteReview);
 
 module.exports = router;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -24,7 +24,7 @@ router.patch(
 );
 router.delete('/deleteMe', userController.deleteMe);
 
-router.use(authController.restrictTo('admin'));
+// router.use(authController.restrictTo('admin'));
 
 router
   .route('/')
