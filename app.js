53d7f7454b9221b94c1eafe03dec2c487411eb9d
SamSin369 <sam.singer79@gmail.com>
2022-07-27 17:39:13 +0700

    done

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
-
+const cors = require('cors');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
