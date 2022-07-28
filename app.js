c1fc9d342452c58036b17d6b9ba1b02c870e517c
SamSin369 <sam.singer79@gmail.com>
2022-07-28 21:26:39 +0700

    new

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ if (process.env.NODE_ENV === 'development') {
 // Limit requests from same API
 const limiter = rateLimit({
   max: 100,
-  windowMs: 60 * 60 * 1000,
+  windowMs: 60 * 60 * 100000,
   message: 'Too many requests from this IP, please try again in an hour!'
 });
 app.use('/api', limiter);
