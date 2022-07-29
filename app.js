b031df08a1009273b1ec9eb32cb0e96c4989476a
SamSin369 <sam.singer79@gmail.com>
2022-07-29 15:42:48 +0700

    server

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ const limiter = rateLimit({
   windowMs: 60 * 60 * 100000,
   message: 'Too many requests from this IP, please try again in an hour!'
 });
-app.use('/api', limiter);
+// app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
